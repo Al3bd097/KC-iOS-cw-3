eee90820891b34d13755c9921fe746bc86dd185e
Al3bd097 <al3bdf@gmail.com>
2022-08-03 18:20:46 +0300

    Done

---
+++ cw3.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Foundation
+
+var name = "Abdullah"
+var favouriteNumber = "17"
+var hobby = "coding"
+let BirthOfDate = "2006"
+
+print("Hello my name is \(name) and my favourite number is \(favouriteNumber) and i love \(hobby) and my birth date is", BirthOfDate)
+
